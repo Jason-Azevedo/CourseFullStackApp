36155d1a314136d7841aaafe4e5c285fb947a863
Jason Azevedo <jasonazevedo03@gmail.com>
2021-03-08 07:00:04 +0200

    Almost finished styling the background of the login page

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -4,20 +4,27 @@ import { Link } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div>
-      <form action="">
-        <h2>Course Todo App</h2>
-        <h1>Welcome to the login page!</h1>
+    <div className="form__background">
+      <div className="form__container">
+        <form className="form" action="">
+          <h2 className="title--small">Course Todo App</h2>
+          <h1 className="title--large">Welcome to the login page!</h1>
 
-        <FormInput inputName="username" errMsg="Invalid username" />
+          <FormInput inputName="username" errMsg="Invalid username" />
 
-        <FormInput inputName="password" errMsg="Invalid password" />
+          <FormInput inputName="password" errMsg="Invalid password" />
 
-        <p>
-          Don't have an account? <Link to="/signup">Make one!</Link>
-        </p>
-        <button type="submit">Login</button>
-      </form>
+          <p className="text">
+            Don't have an account?
+            <Link className="link" to="/signup">
+              Make one!
+            </Link>
+          </p>
+          <button className="btn" type="submit">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
