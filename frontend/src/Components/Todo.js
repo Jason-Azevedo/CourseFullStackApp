89ea2369bd3624bb604edb4ce5bdc2ccb5aed183
Jason Azevedo <jasonazevedo03@gmail.com>
2021-04-05 12:58:54 +0200

    Implemented ability to show the context menu when dots clicked

--- frontend/src/Components/Todo.js
+++ frontend/src/Components/Todo.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Todo() {
+  const [showContextMenu, setShowContextMenu] = useState(false);
+  const showMenu = () => {
+    setShowContextMenu(prev => !prev);
+  }
+
   return (
     <div className="todo">
       <div className="todo__header">
         <h3 className="title--sm">Title</h3>
-        <span className="icon--dots clickable"></span>
-        <div className="todo__context-menu">{/* Options Context Menu */}</div>
+        <span className="icon--dots clickable" onClick={showMenu}></span>
+        <div className={`todo__context-menu ${showContextMenu ? "show" : ""}`}>
+          <p><span className="icon--pencil"></span> Edit</p>
+          <p><span className="icon--trash"></span> Delete</p>
+        </div>
       </div>
 
       <hr />
