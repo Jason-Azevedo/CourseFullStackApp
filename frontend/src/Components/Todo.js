18c2691a619d80c653c72be6a6faa470e383740e
Jason Azevedo <jasonazevedo03@gmail.com>
2021-04-05 15:02:43 +0200

    Added pencil and trash icons to the todo component

--- frontend/src/Components/Todo.js
+++ frontend/src/Components/Todo.js
@@ -1,19 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 
 export default function Todo() {
-  const [showContextMenu, setShowContextMenu] = useState(false);
-  const showMenu = () => {
-    setShowContextMenu(prev => !prev);
-  }
-
   return (
     <div className="todo">
       <div className="todo__header">
         <h3 className="title--sm">Title</h3>
-        <span className="icon--dots clickable" onClick={showMenu}></span>
-        <div className={`todo__context-menu ${showContextMenu ? "show" : ""}`}>
-          <p><span className="icon--pencil"></span> Edit</p>
-          <p><span className="icon--trash"></span> Delete</p>
+        <div>
+          <span className="icon--pencil clickable"></span>
+          <span className="icon--trash clickable"></span>
         </div>
       </div>
 
