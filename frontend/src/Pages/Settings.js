d283cca3163c06532f40fd1e3c836a68ccc0c5f5
Jason Azevedo <jasonazevedo03@gmail.com>
2021-04-16 17:39:49 +0200

    Created the styles for the settings page, not done yet

--- frontend/src/Pages/Settings.js
+++ frontend/src/Pages/Settings.js
@@ -6,12 +6,18 @@ export default function Settings() {
   return (
     <div className="settings-page">
       <div className="container small">
-        <div className="">
+        <div className="settings-header">
           <a className="btn" href="/">
             Back
           </a>
 
-          <h1 className="title--md">[User]'s Settings</h1>
+          <h1 className="title--sm">[User]'s Settings</h1>
+
+          <button className="btn">Save</button>
+        </div>
+        <div className="setting-block">
+          <h2 className="title--xsm">[User]'s Details</h2>
+          <p className="text">Down below is your personal details</p>
         </div>
       </div>
     </div>
