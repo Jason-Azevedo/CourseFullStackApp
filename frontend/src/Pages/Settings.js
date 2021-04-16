e4ee4b4d4dbafbd9d1b045e65fa69851ea2328fd
Jason Azevedo <jasonazevedo03@gmail.com>
2021-04-16 18:12:30 +0200

    Finished implementing the settings page

--- frontend/src/Pages/Settings.js
+++ frontend/src/Pages/Settings.js
@@ -10,14 +10,27 @@ export default function Settings() {
           <a className="btn" href="/">
             Back
           </a>
-
           <h1 className="title--sm">[User]'s Settings</h1>
-
           <button className="btn">Save</button>
         </div>
+
         <div className="setting-block">
           <h2 className="title--xsm">[User]'s Details</h2>
           <p className="text">Down below is your personal details</p>
+
+          <div className="setting-field">
+            <label className="label" htmlFor="username">
+              Username
+            </label>
+            <input className="input" type="text" id="username" />
+          </div>
+
+          <div className="setting-field">
+            <label className="label" htmlFor="username">
+              Password
+            </label>
+            <input className="input" type="text" />
+          </div>
         </div>
       </div>
     </div>
