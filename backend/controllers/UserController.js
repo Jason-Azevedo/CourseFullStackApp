b9851130638caf9b2e20fdacef33dcb3844672dd
Jason Azevedo <jasonazevedo03@gmail.com>
2021-05-04 15:43:29 +0200

    Created UserController

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,12 @@
+exports.login = function (req, res) {
+  // Log the user in!
+  // Then return their updated state {username}
+};
+
+exports.logout = function (req, res) {
+  // Logout
+};
+
+exports.createUser = function (req, res) {};
+exports.editUser = function (req, res) {};
+exports.deleteUser = function (req, res) {};
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,32 +1,18 @@
 const router = require("express").Router();
+const UserController = require("../controllers/UserController");
 
 router.get("/", (req, res) => {
   res.send("This is from the user page!!");
 });
 
-router.get("/login", (req, res) => {
-  // Verify user credentials in body
-  // if successful return their logged in state
-  // ie the username and that they are logged in
-  // else deny it with the following errors:
-  // incorrect credentials
-  // user doesn't exist
-});
+router.post("/login", UserController.login);
 
-router.get("/logout", (req, res) => {
-  // Sign the user out!
-});
+router.get("/logout", UserController.logout);
 
-const createMethod = (req, res) => {
-  // create the new user and redirect them to the login page
-};
-const updateMethod = (req, res) => {
-  // update the user if they are logged in and it is them (no session jacking)
-};
-const deleteMethod = (req, res) => {
-  // delete the user if they are logged in and no session jacking
-};
-
-router.route("/").post(createMethod).patch(updateMethod).delete(deleteMethod);
+router
+  .route("/")
+  .post(UserController.createUser)
+  .patch(UserController.editUser)
+  .delete(UserController.deleteUser);
 
 module.exports = router;
