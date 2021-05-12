b4a73202497a3645dabe5c03eea45e37117818e0
Jason Azevedo <jasonazevedo03@gmail.com>
2021-05-12 18:53:51 +0200

    Implemented create method

--- backend/controllers/TodoController.js
+++ backend/controllers/TodoController.js
@@ -9,6 +9,31 @@ exports.getTodos = function (req, res) {
     });
 };
 
-exports.createTodo = function (req, res) {};
+exports.createTodo = function (req, res) {
+  const newTodo = {
+    userId: req.session.user._id,
+    title: req.body.title,
+    description: req.body.description,
+  };
+
+  if (newTodo.title === undefined || newTodo.title === "") {
+    res.json({ error: "Title of todo is empty" });
+    return;
+  } else if (newTodo.description === undefined || newTodo.description === "") {
+    res.json({ error: "Description of todo is empty" });
+    return;
+  }
+
+  TodoModel.create(newTodo)
+    .then((todo) => {
+      res.json({
+        _id: todo._id,
+        title: todo.title,
+        description: todo.description,
+      });
+    })
+    .catch(() => res.json({ error: "An error occured" }));
+};
+
 exports.editTodo = function (req, res) {};
 exports.deleteTodo = function (req, res) {};
