f53fb418df4f843b0d3b8d80eacce02b1039d704
Jason Azevedo <jasonazevedo03@gmail.com>
2021-05-30 19:08:29 +0200

    Documented usermodel

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -8,19 +8,43 @@ const UserSchema = new mongoose.Schema({
 });
 const UserModel = mongoose.model("User", UserSchema);
 
-// CRUD Methods:
+/**
+ *  Create new user in the database.
+ *
+ * @param   {object} user - The user object.
+ * @returns               - A promise.
+ */
 async function createUser(user) {
   const newUser = await new UserModel(user);
   return newUser.save();
 }
 
+/**
+ *  Edit the user in the database.
+ *
+ * @param   {object} user - The user object.
+ * @returns               - A promise.
+ */
 async function editUser(user) {
   return await UserModel.updateOne({ _id: user._id }, user);
 }
 
+/**
+ *  Fetch the user from the database.
+ *
+ * @param   {object} user - The user object.
+ * @returns               - A promise.
+ */
 async function getUser(username) {
   return await UserModel.findOne({ username: username });
 }
+
+/**
+ *  Delete the user from the database.
+ *
+ * @param   {object} user - The user object.
+ * @returns               - A promise.
+ */
 async function deleteUser(_id) {
   return await UserModel.remove({ _id: _id });
 }
